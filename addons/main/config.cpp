8e21ae7c09720ee2cf86bc94357652cb7205cd5e
Timi007 <timi007gp@gmail.com>
2024-12-21 16:55:36 +0100

    Fix casing

--- addons/main/config.cpp
+++ addons/main/config.cpp
@@ -17,7 +17,7 @@ class CfgSettings {
     class CBA {
         class Versioning {
             class pbw {
-                class dependencies {
+                class Dependencies {
                     CBA[] = {"cba_main", REQUIRED_CBA_VERSION, "true"};
                     ACE[] = {"ace_main", REQUIRED_ACE_VERSION, "true"};
                 };

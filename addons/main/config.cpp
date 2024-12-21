4fafd47175f64f85d31155b653f9d668340b4980
Timi007 <timi007gp@gmail.com>
2024-12-21 16:51:57 +0100

    Fix CBA versioning

--- addons/main/config.cpp
+++ addons/main/config.cpp
@@ -16,7 +16,7 @@ class CfgPatches {
 class CfgSettings {
     class CBA {
         class Versioning {
-            class pbw_main {
+            class pbw {
                 class dependencies {
                     CBA[] = {"cba_main", REQUIRED_CBA_VERSION, "true"};
                     ACE[] = {"ace_main", REQUIRED_ACE_VERSION, "true"};
